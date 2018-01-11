72ce3701d42e4ea32e108f362468266263dc240b
Justine P <justinetulip@gmail.com>
2018-01-10 19:16:08 -0800

    added coprocessor gradle file thing

--- src/main/kotlin/org/team2471/frc/powerup/Robot.kt
+++ src/main/kotlin/org/team2471/frc/powerup/Robot.kt
@@ -2,4 +2,8 @@ package org.team2471.frc.powerup
 
 import edu.wpi.first.wpilibj.IterativeRobot
 
-class Robot : IterativeRobot()
+class Robot : IterativeRobot() {
+    override fun robotInit() {
+        
+    }
+}
