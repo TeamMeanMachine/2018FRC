6a2a349d1b06ebd1048a4f7b3ad39456c8dc1bbc
Justine P <justinetulip@gmail.com>
2018-01-13 16:50:35 -0800

    Added elevator and intake basic stuff. Also robotmap values

--- src/main/kotlin/org/team2471/frc/powerup/Map.kt
+++
@@ -1,4 +0,0 @@
-package org.team2471.frc.powerup
-
-object Map {
-}
---
+++ src/main/kotlin/org/team2471/frc/powerup/RobotMap.kt
@@ -0,0 +1,15 @@
+package org.team2471.frc.powerup
+
+object RobotMap {
+    object Talons {
+        val ELEVATOR_LEFT_BOTTOM_MOTOR = 0
+        val ELEVATOR_RIGHT_BOTTOM_MOTOR = 1
+        val ELEVATOR_LEFT_TOP_MOTOR = 2
+        val ELEVATOR_RIGHT_TOP_MOTOR = 3
+        val ELEVATOR_WRIST_LEFT_MOTOR = 4
+        val ELEVATOR_WRIST_RIGHT_MOTOR = 5
+
+        val INTAKE_LEFT_MOTOR = 6
+        val INTAKE_RIGHT_MOTOR = 7
+    }
+}
