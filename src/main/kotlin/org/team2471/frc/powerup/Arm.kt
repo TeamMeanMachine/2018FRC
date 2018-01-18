115e0b5cdc91f1d301628916f5b406befaff1bca
James-Kling <klingjt@gmail.com>
2018-01-17 20:45:15 -0800

    fixed conflicting issue with talons.

--- src/main/kotlin/org/team2471/frc/powerup/Arm.kt
+++ src/main/kotlin/org/team2471/frc/powerup/Arm.kt
@@ -4,9 +4,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode
 import com.ctre.phoenix.motorcontrol.FeedbackDevice
 import com.ctre.phoenix.motorcontrol.NeutralMode
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
-import org.team2471.frc.lib.control.experimental.Command
-import org.team2471.frc.lib.control.experimental.CommandSystem
-import org.team2471.frc.lib.control.experimental.periodic
 import org.team2471.frc.lib.control.experimental.suspendUntil
 import org.team2471.frc.lib.control.plus
 import kotlin.math.absoluteValue
@@ -16,26 +13,22 @@ object Arm {
 
     private val wristMotors = TalonSRX(RobotMap.Talons.ARM_WRIST_MOTOR_1).apply {
         configSelectedFeedbackSensor(FeedbackDevice.Analog, 0, 10)
+        selectProfileSlot(0, 0)
         setNeutralMode(NeutralMode.Brake)
         config_kP(0, 5.0, 10)
         config_kD(0, 2.0, 10)
     } + TalonSRX(RobotMap.Talons.ARM_WRIST_MOTOR_2)
 
-    private var pivot: Double
-        get() = wristMotors.motorOutputVoltage
-        set(value) = wristMotors.set(ControlMode.PercentOutput, value)
-
-    val position get() = wristMotors.sensorCollection.analogInRaw
-
     init {
-        wristMotors.neutralOutput()
+        wristMotors.set(ControlMode.PercentOutput, 0.0)
     }
 
     suspend fun moveToAngle(angle: Double) {
         val native = degreesToNativeUnits(angle)
         wristMotors.set(ControlMode.Position, native)
-        println("I'm  trying here!: " + angle + " Native: " + native)
-        suspendUntil { wristMotors.getClosedLoopError(0).absoluteValue < degreesToNativeUnits(5.0) }
+        suspendUntil {
+            nativeUnitsToDegrees(wristMotors.getClosedLoopError(0)).absoluteValue < 5.0
+        }
     }
 
     private fun nativeUnitsToDegrees(nativeUnits: Int): Double = (nativeUnits - offset) / (8.0 / 3.0)
