ef63f6a00bb8d952d371a7f7705acf941923233e
tylerscheuble <tylerscheuble@gmail.com>
2018-01-21 19:40:53 -0800

    made autonomous file(only circle and 10 feet)

--- src/main/kotlin/org/team2471/frc/powerup/Robot.kt
+++ src/main/kotlin/org/team2471/frc/powerup/Robot.kt
@@ -1,12 +1,32 @@
 package org.team2471.frc.powerup
 
 import edu.wpi.first.wpilibj.IterativeRobot
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 
 class Robot : IterativeRobot() {
     override fun robotInit() {
         Drive
         Arm
         Intake
+        AutoChooser
     }
 
+    override fun autonomousInit() {
+        AutoChooser.chosenAuto.launch()
+    }
+
+    override fun disabledPeriodic() {
+//        println("Right: " + Drive.rightMotors.sensorCollection.quadraturePosition + ", Left: " + Drive.leftMotors.sensorCollection.quadraturePosition)
+    }
+
+    override fun teleopPeriodic() {
+//        println("Left: " + Drive.leftMotors.getSelectedSensorPosition(0) + ", Right: " + Drive.rightMotors.getSelectedSensorPosition(0))
+
+    }
+
+    override fun autonomousPeriodic() {
+//        println("Left: " + Drive.leftMotors.sensorCollection.quadraturePosition + ", Right: " + Drive.rightMotors.sensorCollection.quadraturePosition)
+//        println("Left Error: " + Drive.leftMotors.getClosedLoopError(0) + ", Right Error: " + Drive.rightMotors.getClosedLoopError(0))
+        println(Drive.ticksToFeet(Drive.leftMotors.sensorCollection.quadraturePosition))
+    }
 }
