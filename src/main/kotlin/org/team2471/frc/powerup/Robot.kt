d1dac978e658d2a3d6cc2b4ddb3a2c6f5532c94b
James-Kling <klingjt@gmail.com>
2018-01-29 16:01:41 -0800

    beanbreak

--- src/main/kotlin/org/team2471/frc/powerup/Robot.kt
+++ src/main/kotlin/org/team2471/frc/powerup/Robot.kt
@@ -2,22 +2,15 @@ package org.team2471.frc.powerup
 
 import edu.wpi.first.wpilibj.AnalogInput
 import edu.wpi.first.wpilibj.IterativeRobot
-import edu.wpi.first.wpilibj.RobotController
-import edu.wpi.first.wpilibj.Solenoid
 
 class Robot : IterativeRobot() {
-    lateinit var ultrasonic: AnalogInput
-    lateinit var solenoid: Solenoid
+    lateinit var beamBreak: AnalogInput
 
     override fun robotInit() {
-        ultrasonic = AnalogInput(0)
-        solenoid = Solenoid(3)
+        beamBreak = AnalogInput(0)
     }
 
-    override fun teleopPeriodic() {
-        val ultrasonicReading = ultrasonic.voltage / 0.2974848
-        println(ultrasonicReading)
-        solenoid.set(RobotController.getUserButton())
-        // 4.88 mV per 5 mm , 4.88 * 60.96 = mV per foot , mV per foot = 297.4848 , V per foot = 0.2974848  MULTIPLY OR DIVIDE???
+    override fun robotPeriodic() {
+        println(beamBreak.voltage)
     }
 }
