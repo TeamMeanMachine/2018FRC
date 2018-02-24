4aa1d12e9cc6b3d02992ec2c3bd81af19590299c
tylerscheuble <tylerscheuble@gmail.com>
2018-02-23 21:23:31 -0800

    HSV Tuning

--- vision_app/app/src/main/java/org/team2471/powerupvision/ImagePreferences.kt
+++ vision_app/app/src/main/java/org/team2471/powerupvision/ImagePreferences.kt
@@ -1,13 +1,12 @@
 package org.team2471.powerupvision
 
 object ImagePreferences {
-    var hueMin: Int = 35
-    var hueMax: Int = 40
-    var satMin: Int = 115
+    var hueMin: Int = 10
+    var hueMax: Int = 35
+    var satMin: Int = 85
     var satMax: Int = 255
-    var valMin: Int = 15
-    var valMax: Int = 225
+    var valMin: Int = 65
+    var valMax: Int = 255
     var exposure: Int = -12
     var displayMode: VisionProcessing.DisplayMode = VisionProcessing.DisplayMode.RAW
-
 }
